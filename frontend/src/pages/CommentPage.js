6803150c81d3dc25d046f17cfac74140a514c449
Nikita <20463298@student.curtin.edu.au>
2024-12-02 22:18:09 +0530

    Updated pages

--- frontend/src/pages/CommentPage.js
+++ frontend/src/pages/CommentPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const HomePage = () => {
+const CommentPage = () => {
   return (
-    <div className="home-page">
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is my creative portfolio website.</p>
-      <p>Explore my projects and feel free to leave a comment!</p>
+    <div className="comment-page">
+
     </div>
   );
 };
 
-export default HomePage;
+export default CommentPage;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is my creative portfolio website.</p>
+      <p>Explore my projects and feel free to leave a comment!</p>
+    </div>
+  );
+};
+
+export default HomePage;
